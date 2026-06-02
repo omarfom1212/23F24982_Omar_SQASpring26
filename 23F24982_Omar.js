400c59ab441468cf8950f336f2cf6a8ac209e16a
omarfom1212 <omarfom1212@gmail.com>
2026-06-02 23:49:38 +0400

    Initial Version - Main File

---
+++ 23F24982_Omar.js
@@ -0,0 +1,29 @@
+let sector = "IT";
+let growthRate = 18;
+let sectorName;
+let demandLevel;
+
+switch (sector) {
+    case "IT":
+        sectorName = "Information Technology";
+        break;
+    case "Healthcare":
+        sectorName = "Healthcare";
+        break;
+    case "Education":
+        sectorName = "Education";
+        break;
+    default:
+        sectorName = "Invalid Sector";
+}
+
+if (growthRate >= 15) {
+    demandLevel = "High Hiring Demand";
+} else if (growthRate >= 5) {
+    demandLevel = "Moderate Hiring Demand";
+} else {
+    demandLevel = "Low Hiring Demand";
+}
+
+console.log("Selected Sector: " + sectorName);
+console.log("Predicted Result: " + demandLevel);
